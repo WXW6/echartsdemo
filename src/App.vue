83a0091be5ce9bd7d7dcc2a37b38a160cf9c836b
D22050707 <D22050707@wistron.com>
2023-04-13 13:33:14 +0800

    最终版本
    
    
    git commit -m 最终版本

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <Page />
+    <!-- <Test></Test> -->
   </div>
 </template>
 
 <script>
 import Page from "./components/Page.vue";
+import Test from "@/components/Test";
 
 export default {
   name: "App",
   components: {
     Page,
+    Test,
   },
 };
 </script>
---
+++ src/components/Test.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div class="rem">1rem</div>
+    <div class="px">80px</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Test",
+};
+</script>
+
+<style lang="scss" scoped>
+.rem {
+  width: 1rem;
+  height: 1rem;
+  background-color: skyblue;
+}
+.px {
+  width: 80px;
+  height: 80px;
+  background-color: pink;
+}
+</style>
